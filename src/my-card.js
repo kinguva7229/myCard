241ef0b05de7613a808da81506e868d2f298d27c
kinguva7229 <kinguva7229@gmail.com>
2025-09-21 21:58:10 -0400

    Update demo index and fix my-card.js

--- src/my-card.js
+++ src/my-card.js
@@ -15,23 +15,75 @@ export class MyCard extends LitElement {
   constructor() {
     super();
     this.title = "My card";
+    this.heading = "card-title";
+    this.image = "Poster Image";
+    this.alt = "Movie poster";
+    this.para = "information";
+    this.poster = "card-image ";
   }
 
   static get styles() {
     return css`
       :host {
-        display: block;
+        display: block; 
       }
+      .wrapper {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 16px;
+  justify-content: center;
+}
+.card {
+  max-width: 400px;
+  border: 2px solid black;
+  padding: 16px;
+  text-align: center;
+  background-color: white;
+}
+.card img {
+  width: 100%;
+  height: auto;
+  margin: 16px 0;
+}
+
+.card.fancy {
+  background-color: DeepSkyBlue;
+}
+@media (max-width: 500px) {
+  .card {
+    max-width: 90%;
+    padding: 12px;
+  }
+  .card img {
+    width: 100%;
+  }
+  .card h1 {
+    font-size: 1.2rem;
+  }
+  .card p {
+    font-size: 0.9rem;
+  }
+}
     `;
   }
 
   render() {
-    return html`<div>${this.title}</div>`;
+    return html`
+  <div class="card">
+    <h1 class="${this.heading}">${this.title}</h1>
+    <img class="${this.poster}" src="${this.image}" alt="${this.alt} poster">
+    <p>${this.para}</p>
+  </div>`;
   }
 
   static get properties() {
     return {
       title: { type: String },
+      heading: { type: String },
+      image: { type: String },
+      alt: { type: String },
+      para: { type: String },
+      poster: { type: String },
     };
   }
 }
